978befa17f2c617b6332cb59bcd71a45c4fb8ad0
TranCuongQ4 <92734523+TranCuongQ4@users.noreply.github.com>
2021-10-24 20:39:58 +0700

    Update menucaidat.sh

--- menucaidat.sh
+++ menucaidat.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-echo "==========Menu=========="
-echo "1. Cài X-UI & Mở Các Port Cần Thiết"
-echo "2. Cài SpeeTest Đo Tốc Độ VPS"
-echo "3. Cài Tăng Tốc VPS"
-echo "4. Thoát"
-echo -n "Chọn Số Để Thực Hiện Chức Năng :"
+echo " ==========Menu========== "
+echo " 1. Cài X-UI & Mở Các Port Cần Thiết "
+echo " 2. Cài SpeeTest Đo Tốc Độ VPS "
+echo " 3. Cài Tăng Tốc VPS "
+echo " 4. Thoát "
+echo -n " Chọn Số Để Thực Hiện Chức Năng : "
 read so
 if [ $so -eq 1 ]
 then
@@ -21,6 +21,6 @@ elif [ $so -eq 4 ]
 then
         exit
 else
-        echo "Bạn Chọn Sai Số Không Có Trong Menu vui Lòng Chọn lại Để Chúng Tôi Phục Vụ Ạ"
+        echo " Bạn Chọn Sai Số Không Có Trong Menu vui Lòng Chọn lại Để Chúng Tôi Phục Vụ Ạ "
         exit
 fi
